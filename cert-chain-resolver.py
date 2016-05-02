27dc2f8ae58294711fe5814e5bed55d6be0290ab
Hubert Chathi <hubert@muchlearning.org>
2016-05-01 21:21:08 -0400

    add option to output separate files for each certificate (Closes #3)

--- cert-chain-resolver.py
+++ cert-chain-resolver.py
@@ -28,6 +28,7 @@ import re
 import urllib2
 import sys
 import subprocess
+import os.path
 
 issuer_re = re.compile('^CA Issuers - URI:(.*)$', re.MULTILINE)
 
@@ -42,6 +43,7 @@ SSL certificate chain resolver
     parser.add_argument("-n", metavar="NUM", required=False, type=int, help="maximum number of certificates to fetch (not including the given certificate)")
     parser.add_argument("-t", "--trusted", metavar="STORE", required=False, help="stop fetching when we find a certificate signed by a trusted CA whose certificate is given in STORE (STORE may contain multiple PEM-format certificates concatenated together)")
     parser.add_argument("--verify", required=False, action="store_true", help="verify the certificate chain after fetching it")
+    parser.add_argument("--separate", required=False, action="store_true", help="create separate files for each certificate (each file will be named based on the name given to the --output option, but with _1, _2, ... inserted before the extension)")
 
     args = parser.parse_args()
     store = None
@@ -69,68 +71,75 @@ SSL certificate chain resolver
                     break
                 line = f.readline()
 
-    with open(args.output, 'w') as outfile:
-        with open(args.input, 'r') as infile:
-            cert_text = infile.read()
-        first = True
-        found = True
-        n = 0
-        certs = []
-        while found:
-            # parse the certificate data
+    if not args.separate:
+        outfile = open(args.output, 'w')
+
+    with open(args.input, 'r') as infile:
+        cert_text = infile.read()
+    first = True
+    found = True
+    n = 0
+    certs = []
+    while found:
+        # parse the certificate data
+        try:
+            cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_text)
+        except:
+            cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, cert_text)
+        sys.stderr.write("%d: %s\n" % (n, cert.get_subject().commonName))
+        certs.append(cert)
+
+        # check and write the certificate
+        if cert.has_expired():
+            sys.stderr.write("Error: Certificate expired")
+            exit(1)
+        if n != 0:
+            if args.separate:
+                root, ext = os.path.splitext(args.output)
+                outfile = open("%s_%d%s" % (root, n, ext), 'w')
+            outfile.write(OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, cert))
+            if args.separate:
+                outfile.close()
+
+        n = n+1
+        # check if we should stop
+        if store:
             try:
-                cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_text)
-            except:
-                cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, cert_text)
-            sys.stderr.write("%d: %s\n" % (n, cert.get_subject().commonName))
-            certs.append(cert)
-
-            # check and write the certificate
-            if cert.has_expired():
-                sys.stderr.write("Error: Certificate expired")
-                exit(1)
-            if n != 0:
-                outfile.write(OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, cert))
-
-            n = n+1
-            # check if we should stop
-            if store:
-                try:
-                    OpenSSL.crypto.X509StoreContext(store, cert).verify_certificate()
-                    sys.stderr.write("Certificate is signed by a trusted CA\n")
-                    break
-                except:
-                    pass
-            if args.n and args.n < n:
+                OpenSSL.crypto.X509StoreContext(store, cert).verify_certificate()
+                sys.stderr.write("Certificate is signed by a trusted CA\n")
                 break
+            except:
+                pass
+        if args.n and args.n < n:
+            break
 
-            # try to fetch the next certificate
-            found = False
-            num_extensions = cert.get_extension_count()
-            for i in range(0,num_extensions-1):
-                extension = cert.get_extension(i)
-                if extension.get_short_name() == "authorityInfoAccess":
-                    aia = str(extension)
-                    m = issuer_re.search(aia)
-                    if m:
-                        found = True
-                        infile = urllib2.urlopen(m.group(1))
-                        contenttype = infile.info().gettype()
-                        cert_text = infile.read()
-                        infile.close()
-                        if contenttype == "application/x-pkcs7-mime":
-                            # HACK: call the openssl cli tool since pyOpenSSL doesn't export the functions to process PKCS#7 data
-                            proc = subprocess.Popen(["openssl", "pkcs7", "-inform", "DER", "-outform", "PEM", "-print_certs"],
+        # try to fetch the next certificate
+        found = False
+        num_extensions = cert.get_extension_count()
+        for i in range(0,num_extensions-1):
+            extension = cert.get_extension(i)
+            if extension.get_short_name() == "authorityInfoAccess":
+                aia = str(extension)
+                m = issuer_re.search(aia)
+                if m:
+                    found = True
+                    infile = urllib2.urlopen(m.group(1))
+                    contenttype = infile.info().gettype()
+                    cert_text = infile.read()
+                    infile.close()
+                    if contenttype == "application/x-pkcs7-mime":
+                        # HACK: call the openssl cli tool since pyOpenSSL doesn't export the functions to process PKCS#7 data
+                        proc = subprocess.Popen(["openssl", "pkcs7", "-inform", "DER", "-outform", "PEM", "-print_certs"],
+                                                stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        out, err = proc.communicate(cert_text)
+                        if proc.returncode != 0:
+                            proc = subprocess.Popen(["openssl", "pkcs7", "-inform", "PEM", "-outform", "PEM", "-print_certs"],
                                                     stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                             out, err = proc.communicate(cert_text)
                             if proc.returncode != 0:
-                                proc = subprocess.Popen(["openssl", "pkcs7", "-inform", "PEM", "-outform", "PEM", "-print_certs"],
-                                                        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                                out, err = proc.communicate(cert_text)
-                                if proc.returncode != 0:
-                                    sys.stderr.write("Invalid PKCS#7 data encountered\n")
-                                    exit(1)
-                            cert_text = out
+                                sys.stderr.write("Invalid PKCS#7 data encountered\n")
+                                exit(1)
+                        cert_text = out
 
     sys.stderr.write("%d certificate(s) found.\n" % (n-1))
 
